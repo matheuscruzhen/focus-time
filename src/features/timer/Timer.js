55c1ca42c6aca39a5f13cc0d5235e67c72bdf7f6
Matheus <matheuscruzhen@gmail.com>
2022-07-03 21:53:17 -0300

    keep the app awake

--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { ProgressBar } from "react-native-paper";
+import { useKeepAwake } from "expo-keep-awake";
 
 import { Countdown } from "../../components/Countdown";
 import { RoundedButton } from "../../components/RoundedButton";
@@ -9,6 +10,8 @@ import { colors } from "../../utils/colors";
 import { spacing } from "../../utils/sizes";
 
 export const Timer = ({ focusSubject }) => {
+  useKeepAwake();
+
   const [minutes, setMinutes] = useState(0.1);
   const [progress, setProgress] = useState(1);
   const [isStarted, setIsStarted] = useState(false);
